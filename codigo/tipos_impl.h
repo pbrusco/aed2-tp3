74e35dcba27f65cb62cf86b65c2ac6e96fe52b47
marianobianchi08 <marianobianchi08@0418f6e8-d58a-11de-b7d7-53e004a27ad6>
2009-12-07 14:22:34 +0000

    Algo de secuencia hecho. Falta Esta, y lo de iteradores.

--- codigo/tipos_impl.h
+++ codigo/tipos_impl.h
@@ -42,17 +42,220 @@ class Secu
 public:
 
     Secu();
+	~Secu();
     Secu(const Secu<T>& s);
     Secu& operator=(const Secu<T> &s);
     Nat longitud();
     void agAtras(const T & elem);
     void agAdelante(const T & elem);
+	bool vacia() const;
     const T & iesimo(Nat i) const;
     void escribir(ostream & os) const;
 
 private:
-    // A completar...
+
+	template <typename T2>
+	struct Nodo
+	{
+		Nodo(T2 e) : dato(e) {}
+
+		T2 dato;
+		Nodo<T2>* siguiente;
+		Nodo<T2>* anterior;
+	};
+
+	Nodo<T> *prim;
+	Nodo<T> *ult;
+	Nat tamanio;
+	void vaciar();
+	void copiarDesde(const Secu<T>& s);
+
+
 };
+
+template <typename T>
+Secu<T>::Secu(){
+
+	prim = NULL;
+    ult = NULL;
+	tamanio = 0;
+}
+
+template <typename T>
+Secu<T>::~Secu(){
+
+	vaciar();
+}
+
+template <typename T>
+Secu<T>::Secu(const Secu<T>& s){
+
+	prim = NULL;
+    ult = NULL;
+    copiarDesde(s);
+}
+
+template <typename T>
+Secu<T>& Secu<T>::operator=(const Secu<T> &s){
+
+	copiarDesde(s);
+}
+
+template <typename T>
+Nat Secu<T>::longitud(){
+
+	return tamanio;
+}
+
+template <typename T>
+void Secu<T>::agAtras(const T & elem){
+
+	Nodo<T>* nuevo = new Nodo<T>(elem);
+	nuevo->anterior = NULL;
+	nuevo->siguiente = NULL;
+	if (not (vacia())){
+		if (prim->dato > elem || ult->dato < elem){
+			if(prim->dato > elem){
+			
+				prim->anterior = nuevo;
+				nuevo->siguiente = prim;
+				prim = nuevo;
+			}
+			else{
+
+				ult->siguiente = nuevo;
+				nuevo->anterior = ult;
+				ult = nuevo;
+			}
+		}
+		else{
+
+			Nodo<T>* aux = ult;
+			while((aux->anterior)->dato > elem)
+				aux = aux->anterior;
+
+			(aux->anterior)->siguiente = nuevo;
+			nuevo->anterior = aux->anterior;
+			nuevo->siguiente = aux;
+			aux->anterior = nuevo;
+		}
+	}
+	else{
+
+		prim = nuevo;
+		ult = nuevo;		
+	}
+	tamanio++;
+
+}
+
+template <typename T>
+void Secu<T>::agAdelante(const T & elem){
+
+	Nodo<T>* nuevo = new Nodo<T>(elem);
+	nuevo->anterior = NULL;
+	nuevo->siguiente = NULL;
+	if (not (vacia())){
+		if (prim->dato > elem || ult->dato < elem){
+			if(prim->dato > elem){
+			
+				prim->anterior = nuevo;
+				nuevo->siguiente = prim;
+				prim = nuevo;
+			}
+			else{
+
+				ult->siguiente = nuevo;
+				nuevo->anterior = ult;
+				ult = nuevo;
+			}
+		}
+		else{
+
+			Nodo<T>* aux = prim;
+			while((aux->siguiente)->dato < elem)
+				aux = aux->siguiente;
+
+			(aux->siguiente)->anterior = nuevo;
+			nuevo->siguiente = aux->siguiente;
+			nuevo->anterior = aux;
+			aux->siguiente = nuevo;
+		}
+	}
+	else{
+
+		prim = nuevo;
+		ult = nuevo;		
+	}
+	tamanio++;
+}
+
+
+
+template <typename T>
+bool Secu<T>::vacia() const{
+
+	return (prim == NULL);
+}
+
+template <typename T>
+const T & Secu<T>::iesimo(Nat i) const{
+
+	Nodo<T>* aux = prim;	
+	for(int j = 0;j<=i;j++){
+
+		aux = aux->siguiente;
+	}
+
+	return &(aux->dato);
+}
+
+template <typename T>
+void Secu<T>::escribir(ostream & os) const{
+
+	os << '[';
+	Nodo<T> *aux = prim;
+	while(aux != ult){
+		os << (aux->dato) << ',';
+		aux = aux->siguiente;
+		}
+	if (not(vacia())) os << (ult->dato);
+	os << ']';
+}
+
+template <class T> 
+void Secu<T>::vaciar(){
+
+    while (not(vacia())){
+        Nodo<T>* aux = prim;
+		prim = prim->siguiente;
+		if(not(vacia()))
+			prim->anterior = NULL;
+		delete aux;
+    }
+}
+
+template <class T>
+void Secu<T>::copiarDesde(const Secu<T>& s) {
+    
+	vaciar();
+    Nodo<T> *aux = s.prim;
+    while(aux != NULL){
+        agAtras(aux->dato);
+        aux = aux->siguiente;
+    }
+}
+
+
+
+template <typename T>
+ostream & operator<<(ostream &os, const Secu<T> & s)
+{
+    s.escribir(os);
+    return os;
+}
+
+
 
 /////////////////////// FIN SECU ///////////////////////
 
@@ -142,6 +345,15 @@ void ArregloDimensionable<T>::escribir(ostream & os) const{
 	os << arreglo[tamanio-1] << ")";
 
 }
+
+template <typename T>
+ostream & operator<<(ostream &os, const ArregloDimensionable<T> & ad)
+{
+    ad.escribir(os);
+    return os;
+}
+
+
 /////////////////////// FIN ARREGLO DIMENSIONABLE ///////////////////////
 
 
@@ -155,18 +367,6 @@ ostream & operator<<(ostream &os, const Conjunto<T> & c)
     return os;
 }
 
-template <typename T>
-ostream & operator<<(ostream &os, const Secu<T> & s)
-{
-    s.escribir(os);
-    return os;
-}
 
-template <typename T>
-ostream & operator<<(ostream &os, const ArregloDimensionable<T> & ad)
-{
-    ad.escribir(os);
-    return os;
-}
 
 #endif
